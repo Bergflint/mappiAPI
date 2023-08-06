0cb0873e64b6151d72b4197da34d79dd5382f3b5
Bergflint <k.flintberg@hotmail.com>
2023-08-07 01:50:02 +0200

    Test med databas

--- mappiAPI/settings.py
+++ mappiAPI/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'mappiAPI.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600) #För att en server sak fatta vilken databas man har
-    #'default': dj_database_url.config(default=os.environ.get('CLEARDB_DATABASE_URL'))
+    #'default': dj_database_url.config(conn_max_age=600) #För att en server sak fatta vilken databas man har
+    'default': dj_database_url.config(default=os.environ.get('CLEARDB_DATABASE_URL'))
 }
 
 #CLEARDB_DATABASE_URL = 'mysql://b754f992ee5737:445faf96@eu-cdbr-west-03.cleardb.net/heroku_7bc8c09b21ae919?reconnect=true'
