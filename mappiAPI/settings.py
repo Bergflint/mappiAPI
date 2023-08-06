b5eac12543da19ca223644a1c5371ccf6ebf3192
Bergflint <k.flintberg@hotmail.com>
2023-08-07 01:31:39 +0200

    Test med databas

--- mappiAPI/settings.py
+++ mappiAPI/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +77,12 @@ WSGI_APPLICATION = 'mappiAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': dj_database_url.config(conn_max_age=600) #För att en server sak fatta vilken databas man har
-#}
+DATABASES = {
+    #'default': dj_database_url.config(conn_max_age=600) #För att en server sak fatta vilken databas man har
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
-CLEARDB_DATABASE_URL = 'mysql://b754f992ee5737:445faf96@eu-cdbr-west-03.cleardb.net/heroku_7bc8c09b21ae919?reconnect=true'
+#CLEARDB_DATABASE_URL = 'mysql://b754f992ee5737:445faf96@eu-cdbr-west-03.cleardb.net/heroku_7bc8c09b21ae919?reconnect=true'
 
 
 # Password validation
