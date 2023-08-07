3fb2459cfef7dca9b28cae9dbbb32f952a04c95e
Bergflint <k.flintberg@hotmail.com>
2023-08-07 13:24:33 +0200

    La till nödvändig kod i admin.py för att kunna ändra databasen i damin interface

--- subjects/admin.py
+++ subjects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Register your models here.¨
+
+admin.site.register(Course)
+admin.site.register(Field)
+admin.site.register(Task)
